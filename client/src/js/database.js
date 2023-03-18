a55aadbdc5683bf1070d70399a447b9aec6af142
Konny P Ho <conniebilac@gmail.com>
2023-03-17 21:37:12 -0400

    changed port/still working on client js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,29 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Take updated content and save it to IndexedDB.
+export const putDb = async (content) => {
+  const jateDB = await openDB('jate', 1);
+  const tx = jateDB.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  console.log('Data successfully saved to database');
+};
+
+// Get all content from database
+export const getDb = async () => {
+  const jateDB = await openDB('jate', 1);
+  const tx = jateDB.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  result
+    ? console.log('Data retrieved from database')
+    : console.log('Data not found in the database.');
+
+  return result?.value;
+};
 
 initdb();
